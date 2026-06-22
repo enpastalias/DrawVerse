7b0616b2952f813f52cb3546ed025b2e8816be37
Abhishek A R <abhishekar403@gmail.com>
2026-06-22 23:51:49 +0530

    Add environment variable template

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ import dns from 'dns';
 export const connectDB = async () => {
     try {
         dns.setServers(['8.8.8.8', '8.8.4.4']);
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/drawversedb');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/drawversedb');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
